17d0eb945c26a22647ab91cdacf8359704888bab
Thomas Davidoff <68920095+thomasdavidoff@users.noreply.github.com>
2022-05-30 15:09:07 -0400

    Added catch for zero solution conundrum
    Updated main.py to include print statements at output of main function, and added functionality to detect and handle cases wherein there are no available solutions

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-import json, requests
-words = list(json.loads(requests.get('https://raw.githubusercontent.com/dwyl/english-words/master/words_dictionary.json').text).keys())
+import json
+import requests
 
-def check_letters(word_letter_list,conundrum_list):
+words = list(json.loads(
+    requests.get('https://raw.githubusercontent.com/dwyl/english-words/master/words_dictionary.json').text).keys())
+
+
+def check_letters(word_letter_list, conundrum_list):
     letter_checks = []
-    for l in word_letter_list:
-        if (l in conundrum_list) & (word_letter_list.count(l) <= conundrum_list.count(l)):
+    for letter in word_letter_list:
+        if (letter in conundrum_list) & (word_letter_list.count(letter) <= conundrum_list.count(letter)):
             letter_checks.append(True)
         else:
             letter_checks.append(False)
@@ -13,18 +17,29 @@ def check_letters(word_letter_list,conundrum_list):
     else:
         return False
 
-def find_solutions(conundrum,num_solutions=1):
-    words_list = [[char for char in w] for w in words if len(w)<=len(conundrum)]
+
+def solve(conundrum, num_solutions=1):
+    words_list = [[char for char in w] for w in words if len(w) <= len(conundrum)]
     conundrum_list = [char for char in conundrum]
     possible_words = []
     for word in words_list:
-        if check_letters(word,conundrum_list) == True:
+        if check_letters(word, conundrum_list):
             possible_words.append(''.join(word))
     solutions = []
-    for n in range(0,num_solutions):
-        guess = max(possible_words, key=len)
-        possible_words.pop(possible_words.index(guess))
-        solutions.append(guess)
+    if len(possible_words) > 0:
+        for n in range(0, min(num_solutions, len(possible_words))):
+            guess = max(possible_words, key=len)
+            possible_words.pop(possible_words.index(guess))
+            solutions.append(guess)
+
+    longest = max(solutions, key=len) if len(solutions) > 0 else []
+    if len(longest) == len(conundrum):
+        print(f'Conundrum has a perfect solution.\nThe solution is {longest}')
+    elif len(solutions) == 0:
+        print('There is no solution.')
+    else:
+        print(f"Conundrum doesn't have a perfect solution.\nThe best solution is {longest}")
     return solutions
 
-find_solutions('countdown',100)
+
+solve('m', 2)
